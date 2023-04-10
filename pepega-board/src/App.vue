12b8e3b5f2c0619e63762733cf5d6737df8bd49a
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2023-04-10 02:40:00 -0700

    frontend init

---
+++ pepega-board/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+//import HelloWorld from "./components/HelloWorld.vue";
+import PostVue from "./components/Post.vue";
+</script>
+
+<template>
+    <div>
+        <post-vue title="Pepega games">
+            This is a test post
+        </post-vue>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ pepega-board/src/components/Post.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const props = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div class="post">
+        <h1>{{ title }}</h1>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style>
+.post {
+    padding: 8px;
+}
+
+.post > h1 {
+    font-weight: 700;
+}
+</style>
